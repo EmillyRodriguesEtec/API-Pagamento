c0e9f80c393dbf31e82380dd13aed6dee47ee4cd
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test>
2023-06-19 09:23:58 -0300

    Correções

--- Controllers/PagamentoController.cs
+++ Controllers/PagamentoController.cs
@@ -3,6 +3,8 @@ using Newtonsoft.Json;
 using System; 
 using System.Collections.Generic;
 using API_Pagamento.Models;
+using API_Pagamento.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API_Pagamento.Controllers;
 
@@ -11,30 +13,39 @@ namespace API_Pagamento.Controllers;
 
 public class PagamentoController : ControllerBase
 {
+    private readonly DataContext _context;
 
-    private static List<PagamentoPix> pagamentos = new List<PagamentoPix>(); 
+    public PagamentoController(DataContext context)
+    {   
+        _context = context;        
+    }
 
-    [HttpGet] 
+    
+
+    /*[HttpGet("GetAll")] 
     public IActionResult GetPagamentos() 
     { 
-        return Ok(pagamentos); 
-    } 
+        return Ok(_context.PagamentosPix.ToListAsync()); 
+    } */
 
     [HttpPost] 
-    public IActionResult RealizarPagamento(PagamentoPix pagamento) 
+    public async Task<IActionResult> RealizarPagamento(PagamentoPix pagamento) 
     { 
         pagamento.id_pagamento = Guid.NewGuid().ToString(); 
         pagamento.data_pagamento = DateTime.Now; 
         
         // Lógica para processar o pagamento via Pix 
-        pagamentos.Add(pagamento); 
+        await _context.PagamentosPix.AddAsync(pagamento); 
+        await _context.SaveChangesAsync();
+
+
         return CreatedAtAction(nameof(ObterComprovante), new { idPagamento = pagamento.id_pagamento }, pagamento); 
     } 
 
-    [HttpGet("{idPagamento}/comprovante")] 
+    [HttpGet("{idPagamento}")] 
     public IActionResult ObterComprovante(string idPagamento) 
     { 
-        var pagamento = pagamentos.Find(p => p.id_pagamento == idPagamento); 
+        var pagamento = _context.PagamentosPix.FirstOrDefault(p => p.id_pagamento == idPagamento); 
         if (pagamento == null) 
         { 
             return NotFound(); 
@@ -42,4 +53,15 @@ public class PagamentoController : ControllerBase
         var comprovante = JsonConvert.SerializeObject(pagamento); 
         return Ok(comprovante); 
     }               
+
+    [HttpGet("GetAll")] 
+    public async Task<IActionResult> ObterTodos(string idPagamento) 
+    { 
+        var pagamento = await _context.PagamentosPix.ToListAsync(); 
+        if (pagamento == null) 
+        { 
+            return NotFound(); 
+        }         
+        return Ok(pagamento); 
+    }            
 }
